6d9c11313889a3c34db26cb66349537e811d4c27
programando <jhonjamesmg@hotmail.com>
2020-06-17 19:32:23 -0500

    pdf

--- app/Listeners/InvoiceSendXmlPdfToCustomer.php
+++ app/Listeners/InvoiceSendXmlPdfToCustomer.php
@@ -14,7 +14,6 @@ class InvoiceSendXmlPdfToCustomer
         $Emails = $this->getAcountsToSendEmail ( $event->Factura['emails'] );
         $when   = now()->addSeconds(5);
         Mail::to( $Emails )
-                 ->cc('jhonjamesmg@hotmail.com')
                   ->later( $when,new InvoiceSendToCustomerMail(
                             $event->Factura ,
                             $event->FilePdf, $event->FileXml, 
