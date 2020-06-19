717610970510f642e5c85487aa65290a3676ee32
programando <jhonjamesmg@hotmail.com>
2020-06-19 17:24:18 -0500

    Ruta adicional

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 Route::post('/register', 'UserController@register');
 Route::post('/login', 'UserController@login');
 Route::post('/logout', 'UserController@logout'); 
+
+
+Route::get('/facturas', function ( ) {
+    return 'ok';
+});
+
 
 Route::get('invoices','Api\FctrasElctrncasInvoicesController@invoices')->name('invoices');
 
