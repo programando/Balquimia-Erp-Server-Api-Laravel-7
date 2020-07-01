5736327538ad479c5122773645a5a516c1bf5585
programando <jhonjamesmg@hotmail.com>
2020-07-01 10:31:15 -0500

    Seguimiento

--- app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
+++ app/Http/Controllers/Api/FctrasElctrncasInvoicesController.php
@@ -35,7 +35,7 @@ class FctrasElctrncasInvoicesController extends ApiController
             $Documentos = FctrasElctrnca::InvoicesToSend()->get();
             foreach ($Documentos as $Documento ) {
                 $this->invoicesToSend ( $Documento) ;
-                //return $this->jsonObject ;
+                return $this->jsonObject ;
                 $response   = $this->ApiSoenac->postRequest( $URL, $this->jsonObject ) ;          
                 $this->traitUpdateJsonObject ( $Documento );
                 $this->documentsProcessReponse( $Documento, $response ) ;
