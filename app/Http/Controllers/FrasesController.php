8929cf26cf10c1a606466aab70e41f1014e57073
programando <jhonjamesmg@hotmail.com>
2020-07-26 08:37:22 -0500

    Frases controller

--- app/Http/Controllers/FrasesController.php
+++ app/Http/Controllers/FrasesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Frase as Frases;
 
-class FrasesController extends ApiController
+class FrasesController extends Controller
 {
     public function sentenceToday() {
         return  Frases::fraseDelDia();
