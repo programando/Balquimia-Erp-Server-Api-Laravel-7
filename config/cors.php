bf1ca328385c489c4552ba5b534af9e097338f29
programando <jhonjamesmg@hotmail.com>
2020-10-05 19:55:59 -0500

    Cors - Domain update

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths'                    => ['*','/login','/logout','/reset/password','/update/password','/sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:3000','https://facturas.plusoft.co' ],
+    'allowed_origins'          => ['http://localhost:3000','https://ventas.balquimia.com' ],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => false,
