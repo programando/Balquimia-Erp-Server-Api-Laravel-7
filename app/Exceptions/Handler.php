4223f01371965b33c620d9a9380a9d36e33eb45e
programando <jhonjamesmg@hotmail.com>
2020-10-26 19:57:46 -0500

    Error Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -92,7 +92,7 @@ class Handler extends ExceptionHandler
         if ( config('app.debug')) {
             return parent::render($request, $exception);
         }
-        return parent::render($request, $exception);
+        //return parent::render($request, $exception);
         return $this->errorResponse('Falla inesperada. Intente luego', 500);
     }
 
