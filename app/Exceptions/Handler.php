f4d2324432b5c3b095071742a57647af14be039b
programando <jhonjamesmg@hotmail.com>
2020-10-26 19:52:47 -0500

    Error - Controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -92,7 +92,8 @@ class Handler extends ExceptionHandler
         if ( config('app.debug')) {
             return parent::render($request, $exception);
         }
-        //return $this->errorResponse('Falla inesperada. Intente luego', 500);
+        return parent::render($request, $exception);
+        return $this->errorResponse('Falla inesperada. Intente luego', 500);
     }
 
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
